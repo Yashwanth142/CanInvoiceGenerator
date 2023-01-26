0a1c665a3815ea7643044ff407f4ebb1b688855e
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-26 17:36:40 +0530

    Main Program

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to cab Invoice Generator");
+
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+
+            //double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+
+            InvoiceSummary invoiceSummary =invoiceGenerator.CalculateFare(rides);
+
+           
+        }
+    }
+}
